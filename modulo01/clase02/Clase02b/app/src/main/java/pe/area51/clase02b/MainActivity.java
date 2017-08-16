c222a92f62d670d1567a365844de8110b5e62ba7
segundo_acosta <sacosta@platcel.com>
2017-08-15 20:48:54 -0500

    Clase02b

---
+++ modulo01/clase02/Clase02b/app/src/main/java/pe/area51/clase02b/MainActivity.java
@@ -0,0 +1,17 @@
+package pe.area51.clase02b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
