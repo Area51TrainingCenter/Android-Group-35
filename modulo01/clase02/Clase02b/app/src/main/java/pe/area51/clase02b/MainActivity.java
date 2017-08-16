5e3b60ce360e62729df6b1744019cf036769ef34
segundo_acosta <sacosta@platcel.com>
2017-08-15 21:53:36 -0500

    event click

--- modulo01/clase02/Clase02b/app/src/main/java/pe/area51/clase02b/MainActivity.java
+++ modulo01/clase02/Clase02b/app/src/main/java/pe/area51/clase02b/MainActivity.java
@@ -1,17 +1,76 @@
 package pe.area51.clase02b;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txt;
+    TextView title;
+    Button btn;
+
+    //Contexto o momento de vida de la Actividad
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
+
+        //Relacionamos las variables a los componentes visuales
+        title = (TextView) findViewById(R.id.title);
+        txt = (EditText) findViewById(R.id.txt);
+        btn = (Button) findViewById(R.id.btn);
+
+
+        //btn.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String cadena = txt.getText().toString();
+
+                if (!cadena.equals("")) {
+                    title.setText(cadena);
+                } else {
+                    /*
+                    Toast.makeText(context,
+                            getResources().getString(R.string.welcome_error)
+                            , Toast.LENGTH_SHORT);
+                    */
+
+
+                    Toast.makeText(context,
+                            R.string.welcome_error
+                            , Toast.LENGTH_SHORT);
+
+                }
+
+
+            }
+        });
 
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    /*
+    @Override
+    public void onClick(View view) {
+
+    }
+    */
 }
