685ae3ca7da3e5b7b3fcc981fa517ae19fbea97a
segundo_acosta <sacosta@platcel.com>
2017-08-15 20:00:41 -0500

    Clase Log

--- modulo01/clase02/Clase02/app/src/main/java/pe/area51/clase02/MainActivity.java
+++ modulo01/clase02/Clase02/app/src/main/java/pe/area51/clase02/MainActivity.java
@@ -2,13 +2,28 @@ package pe.area51.clase02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+
+//import pe.area51.clase02.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String tag = "Clase02";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("","");
+
+
+
+
+
+
     }
 
     @Override
---
+++ modulo01/clase02/Clase02/app/src/main/java/pe/area51/clase02/util/Log.java
@@ -0,0 +1,18 @@
+package pe.area51.clase02.util;
+
+/**
+ * Created by segundo on 15/08/17.
+ */
+
+public class Log {
+
+    public void holaSoyElLog(){
+
+    }
+
+
+    public void d(String a, String b){
+
+    }
+
+}
