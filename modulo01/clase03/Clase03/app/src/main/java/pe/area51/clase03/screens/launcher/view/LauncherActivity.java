c0c1f0d5bf56c95f2d76c2e0413a9e02495be0ce
segundo_acosta <sacosta@platcel.com>
2017-08-17 21:41:25 -0500

    animations activity

--- modulo01/clase03/Clase03/app/src/main/java/pe/area51/clase03/screens/launcher/view/LauncherActivity.java
+++ modulo01/clase03/Clase03/app/src/main/java/pe/area51/clase03/screens/launcher/view/LauncherActivity.java
@@ -1,9 +1,12 @@
 package pe.area51.clase03.screens.launcher.view;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import pe.area51.clase03.R;
+import pe.area51.clase03.screens.welcome.view.WelcomeActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -12,5 +15,23 @@ public class LauncherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_launcher);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toWelcome();
+            }
+        }, 2000);
+
     }
+
+    public void toWelcome() {
+
+        Intent intent = new Intent(LauncherActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+        //finish();
+
+    }
+
 }
