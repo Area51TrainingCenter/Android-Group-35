08196d69b450c358b6c989dfbe62438fc034ea3d
segundo_acosta <sacosta@platcel.com>
2017-08-24 19:32:58 -0500

    zip

---
+++ modulo01/clase05/Clase05/app/src/main/java/pe/area51/clase05/screens/home/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.area51.clase05.screens.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.area51.clase05.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ modulo01/clase05/Clase05/app/src/main/java/pe/area51/clase05/screens/launcher/LauncherActivity.java
@@ -0,0 +1,15 @@
+package pe.area51.clase05.screens.launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.area51.clase05.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
