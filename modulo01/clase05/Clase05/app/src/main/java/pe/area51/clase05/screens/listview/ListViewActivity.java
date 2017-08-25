524dd55c4b65a6faf2ba27c873f448036ba86908
segundo_acosta <sacosta@platcel.com>
2017-08-24 22:03:46 -0500

    Clase05

--- modulo01/clase05/Clase05/app/src/main/java/pe/area51/clase05/screens/listview/ListViewActivity.java
+++ modulo01/clase05/Clase05/app/src/main/java/pe/area51/clase05/screens/listview/ListViewActivity.java
@@ -1,15 +1,51 @@
 package pe.area51.clase05.screens.listview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import pe.area51.clase05.R;
+import pe.area51.clase05.models.PersonModel;
+import pe.area51.clase05.screens.gridview.GridViewAdapter;
 
 public class ListViewActivity extends AppCompatActivity {
+
+
+
+    GridViewAdapter adapter;
+    ListView people;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        context = this;
+
+        people = (ListView) findViewById(R.id.people);
+
+
+        ArrayList<PersonModel> arreglo = new ArrayList<PersonModel>();
+        for (int i = 1; i < 30; i++) {
+            PersonModel pm = new PersonModel();
+            pm.setPhoto("http://segundoacosta.com/people/img_" + i + ".jpg");
+
+            arreglo.add(pm);
+        }
+
+
+        adapter = new GridViewAdapter(context, arreglo);
+        people.setAdapter(adapter);
+
+
     }
+
+
+
 }
