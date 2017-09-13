0dd2e47c59e07623814ee685e0f49c763bda7efb
segundo_acosta <sacosta@platcel.com>
2017-09-12 19:30:20 -0500

    toLogin

--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/splash/view/SplashActivity.java
+++ modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/splash/view/SplashActivity.java
@@ -1,9 +1,13 @@
 package pe.area51.socialapp.screens.splash.view;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import pe.area51.socialapp.R;
+import pe.area51.socialapp.screens.home.view.HomeActivity;
+import pe.area51.socialapp.screens.login.view.LoginActivity;
 import pe.area51.socialapp.widgets.activity.SocialAppActivity;
 
 public class SplashActivity extends SocialAppActivity {
@@ -12,5 +16,29 @@ public class SplashActivity extends SocialAppActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toLogin();
+            }
+        }, 2000);
+
     }
+
+    void toHome() {
+        Intent intent = new Intent(SplashActivity.this,
+                HomeActivity.class);
+        startActivity(intent);
+    }
+
+    void toLogin() {
+        Intent intent = new Intent(SplashActivity.this,
+                LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
