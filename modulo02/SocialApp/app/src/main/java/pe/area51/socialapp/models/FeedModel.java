cdec9c66abba268171599c045f617bac46ecf02b
segundo_acosta <sacosta@platcel.com>
2017-09-14 20:56:11 -0500

    add item

---
+++ modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/models/FeedModel.java
@@ -0,0 +1,55 @@
+package pe.area51.socialapp.models;
+
+/**
+ * Created by segundo on 14/09/17.
+ */
+
+public class FeedModel {
+
+    private String id;
+    private String photo;
+    private String title;
+    private int comments;
+    private int favourites;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    public int getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(int favourites) {
+        this.favourites = favourites;
+    }
+}
