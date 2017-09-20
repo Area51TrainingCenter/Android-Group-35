13f7e64fccf560f63b47704ff233ccb016bc8c03
segundo_acosta <sacosta@platcel.com>
2017-09-19 21:55:51 -0500

    Implement Session

--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/helpers/session/SocialAppSession.java
+++ modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/helpers/session/SocialAppSession.java
@@ -1,9 +1,33 @@
 package pe.area51.socialapp.helpers.session;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by segundo on 7/09/17.
  */
 
 public class SocialAppSession {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    String name_pref = "SocialApp";
+    String state_pref = "state";
+
+    public SocialAppSession(Context context) {
+
+        preferences = context.getSharedPreferences(name_pref, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+    }
+
+    public boolean isLogin() {
+
+        return preferences.getBoolean(state_pref, false);
+
+    }
+
 
 }
