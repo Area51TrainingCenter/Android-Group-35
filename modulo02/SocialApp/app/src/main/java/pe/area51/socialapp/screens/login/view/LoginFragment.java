3e049ddb8dcee597c915ee484edd05941faa1a4f
segundo_acosta <sacosta@platcel.com>
2017-09-26 20:45:51 -0500

    add validation

--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/login/view/LoginFragment.java
+++ modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/login/view/LoginFragment.java
@@ -2,10 +2,13 @@ package pe.area51.socialapp.screens.login.view;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import pe.area51.socialapp.R;
 
@@ -13,6 +16,10 @@ import pe.area51.socialapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+
+    View view;
+    EditText txtpassword, txtemail;
+    Button btnlogin;
 
     public LoginFragment() {
 
@@ -23,8 +30,23 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        txtemail = (EditText) view.findViewById(R.id.txtemail);
+        txtpassword = (EditText) view.findViewById(R.id.txtpassword);
+        btnlogin = (Button) view.findViewById(R.id.btnlogin);
+
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+
+
+    }
 }
--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/login/view/SignUpFragment.java
+++ modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/login/view/SignUpFragment.java
@@ -1,18 +1,27 @@
 package pe.area51.socialapp.screens.login.view;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import pe.area51.socialapp.R;
+import pe.area51.socialapp.databinding.FragmentSignUpBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment {
+
+
+    FragmentSignUpBinding binding;
+
+    int error_code = 0;
 
 
     public SignUpFragment() {
@@ -23,8 +32,88 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up,
+                container, false);
+
+        return binding.getRoot();
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        binding.btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (validate()) {
+                    //Campos validos, se procede al registro
+
+                }else{
+                    //Mostramos mensaje de error
+
+                    switch (error_code){
+
+                    }
+
+                }
+
+
+            }
+        });
+
+
+
+    }
+
+    public boolean validate() {
+
+        boolean state = false;
+
+        if (!binding.txtname.getText().toString().equals("")
+                && binding.txtname.length() > 3) {
+            state = true;
+        }else{
+            error_code = 1;
+        }
+
+        if (!binding.txtlastname.getText().toString().equals("")
+                && binding.txtlastname.length() > 3) {
+            state = true;
+        }else{
+            error_code = 2;
+        }
+
+        if (!binding.txtemail.getText().toString().equals("")
+                && validateEmail(binding.txtemail.getText().toString())) {
+            state = true;
+        }else{
+            error_code = 3;
+        }
+
+
+        if (!binding.txtpassword.getText().toString().equals("")
+                && !binding.txtpassword2.getText().toString().equals("")
+                && binding.txtpassword.getText().toString()
+                .equals(binding.txtpassword2.getText().toString())
+                ) {
+
+            state = true;
+        }else{
+            error_code = 4;
+        }
+
+
+        return state;
+    }
+
+
+    public boolean validateEmail(String email) {
+        return !TextUtils.isEmpty(email) &&
+                android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
 
 }
