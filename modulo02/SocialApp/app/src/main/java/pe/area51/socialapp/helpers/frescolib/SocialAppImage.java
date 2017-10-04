4aea05bb22cc6ee72a8d4bfc235fcc845467738e
segundo_acosta <sacosta@platcel.com>
2017-10-03 19:18:27 -0500

    delete app

--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/helpers/frescolib/SocialAppImage.java
+++
@@ -1,9 +0,0 @@
-package pe.area51.socialapp.helpers.frescolib;
-
-/**
- * Created by segundo on 7/09/17.
- */
-
-public class SocialAppImage {
-
-}
--- modulo02/SocialApp/app/src/main/java/pe/area51/socialapp/screens/login/viewmodel/LoginFragViewModel.java
+++
@@ -1,8 +0,0 @@
-package pe.area51.socialapp.screens.login.viewmodel;
-
-/**
- * Created by segundo on 14/09/17.
- */
-
-public class LoginFragViewModel {
-}
