b7f737246110284b5619ee9abdc8c9c6392a1924
segundo_acosta <sacosta@platcel.com>
2017-10-17 20:31:38 -0500

    add fcm

---
+++ modulo03/SocialApp/app/src/main/java/pe/area51/socialapp/helpers/firebase/notifications/SocialAppInstanceIDService.java
@@ -0,0 +1,13 @@
+package pe.area51.socialapp.helpers.firebase.notifications;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by segundo on 17/10/17.
+ */
+
+public class SocialAppInstanceIDService extends FirebaseInstanceIdService {
+
+
+
+}
---
+++ modulo03/SocialApp/app/src/main/java/pe/area51/socialapp/helpers/firebase/notifications/SocialAppMessagingService.java
@@ -0,0 +1,13 @@
+package pe.area51.socialapp.helpers.firebase.notifications;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by segundo on 17/10/17.
+ */
+
+public class SocialAppMessagingService extends FirebaseMessagingService {
+
+
+
+}
